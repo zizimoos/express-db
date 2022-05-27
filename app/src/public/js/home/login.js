450ba8ce7bf01045f305fc3e38201b3872d10579
남형석 <azerc@MacMini.local>
2022-05-27 14:58:22 +0900

    take html object using DOM

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,1 +1,14 @@
-console.log("login.js");
+const id = document.getElementById("id");
+const psword = document.getElementById("psword");
+const loginBtn = document.querySelector("button");
+
+function login() {
+  const idValue = id.value;
+  const pswordValue = psword.value;
+  const req = {
+    id: idValue,
+    psword: pswordValue,
+  };
+  console.log(req);
+}
+loginBtn.addEventListener("click", login);
